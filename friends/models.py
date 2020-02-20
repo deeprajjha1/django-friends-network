8e7127764e38bcaaae98a4fb2d5ab88a31643c3a
Deepraj jha <jha.deepraj@gmail.com>
2020-02-21 00:37:01 +0530

    Update models.py

--- friends/models.py
+++ friends/models.py
@@ -15,22 +15,3 @@ class Friendship(models.Model):
 
   class Meta:
       unique_together = (('to_friend', 'from_friend'), )
-
-"""
-class Friends :
-    title = models.CharField(max_length=100,blank=True,default='')
-    friends = models.ManyToManyField(User)
-
--------------------------------------------------------------------------------------------------
-
-class Person(AbstractBaseUser, PermissionsMixin):
-    relationships = models.ManyToManyField('self', through='Relationship',
-                                           symmetrical=False,
-                                           related_name='related_to')
-
-
-class Relationship(models.Model):
-    from_person = models.ForeignKey(Person, related_name='from_people')
-    to_person = models.ForeignKey(Person, related_name='to_people')
-
-"""
