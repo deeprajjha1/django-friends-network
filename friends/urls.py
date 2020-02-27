587d3b3ea710e7d348664266e1b4ba7859777f6c
Deepraj jha <jha.deepraj@gmail.com>
2020-02-28 02:11:10 +0530

    Update urls.py

--- friends/urls.py
+++ friends/urls.py
@@ -4,6 +4,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     path('friends/<int:id>/',views.friend_list),
+    path('users/',views.user_list),
+    path('users/<int:id>/',views.user),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
